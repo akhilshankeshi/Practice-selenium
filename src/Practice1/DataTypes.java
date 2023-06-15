7652a908c2f4597a09359148df3b3d9f7e0aca0e
akhil <akhil@DESKTOP-4LDV9MD>
2023-06-15 18:08:15 +0530

    Data Types

---
+++ src/Practice1/DataTypes.java
@@ -0,0 +1,22 @@
+package Practice1;
+
+public class DataTypes {
+     public static void main(String[] args) {
+    	 int a=25;
+         float b=2.5f;
+         char c='a';
+         String d="Akhil";
+         boolean e= true;
+         
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+         System.out.println(d);
+         System.out.println(e);
+         
+	}
+     
+
+     
+     
+}
