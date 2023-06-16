be8b496b265d61545405545d0b1c84b35f3441ac
akhil <akhil@DESKTOP-4LDV9MD>
2023-06-16 21:12:32 +0530

    Operators

--- src/Practice1/Operators.java
+++ src/Practice1/Operators.java
@@ -12,6 +12,20 @@ public static void main(String[] args) {
 	System.out.println(a*b);
 	System.out.println(a/b);
 	System.out.println(a%b);
+	
+	
+	//Comparison Operator
+	
+	int i=15;
+	int j=20;
+	
+	System.out.println();
+	System.out.println(i>j);
+	System.out.println(i<j);
+	System.out.println(i==j);
+	System.out.println(i>=j);
+	System.out.println(i<=j);
+	System.out.println(i!=j);
 }
 
 }
