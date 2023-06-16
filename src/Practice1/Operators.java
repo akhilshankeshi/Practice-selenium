4116e9d7700f2f15614142871924462c09b1812a
akhil <akhil@DESKTOP-4LDV9MD>
2023-06-16 20:39:03 +0530

    Operators

---
+++ src/Practice1/Operators.java
@@ -0,0 +1,17 @@
+package Practice1;
+
+public class Operators {
+public static void main(String[] args) {
+	//Arithmetic Operator
+	
+	int a=50;
+	int b=20;
+	
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	System.out.println(a%b);
+}
+
+}
